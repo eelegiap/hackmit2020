d984cd2f7be2664649a2943a677cf9047c29960f
Paige Lee <paigelee@Paiges-MacBook-2.local>
2020-09-19 22:02:32 -0400

    linked map and other changes

--- website-template/js/linkedmap.js
+++ website-template/js/linkedmap.js
@@ -694,9 +694,9 @@ if (Modernizr.svg) {
 
 
     queue()
-        .defer(d3.json, "usa-states1.json")
-        .defer(d3.json, "config.json")
-        .defer(d3.csv, "data1.csv")
+        .defer(d3.json, "data/usa-states1.json")
+        .defer(d3.json, "data/config.json")
+        .defer(d3.csv, "data/data1.csv")
         .await(ready);
 
     function ready(error, geog, config, data) {
